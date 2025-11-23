816343113e7e7298aa7dda83d676887496cd2481
jlamillag <jersahin@gmail.com>
2025-11-22 22:17:45 -0500

    chore: Configuracion de Vite para GitHub Pages y archivos de build

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Debemos especificar la ruta base: el nombre del repositorio
+export default defineConfig({
+  plugins: [react()],
+  // Asegúrate de que 'nini-tamagotchi-v2' sea exactamente el nombre de tu repositorio
+  base: '/nini-tamagotchi-v2/', 
+})
