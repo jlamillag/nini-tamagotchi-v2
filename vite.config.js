67533d82ae48f6f12df076b4bdc51fac65fb3d1b
jlamillag <jersahin@gmail.com>
2025-11-22 23:33:12 -0500

    fix: Corregir sintaxis del script deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Asegúrate de que 'nini-tamagotchi-v2' sea exactamente el nombre de tu repositorio
-  base: '/nini-tamagotchi-v2/', 
+  base: './', // <-- ¡CORREGIR ESTA LÍNEA!
 })
